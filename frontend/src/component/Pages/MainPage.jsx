1305ace60db62a21defcf50059dc30d57106259d
gyeongjun <kanell0304@gmail.com>
2025-12-11 17:34:58 +0900

    test

--- frontend/src/component/Pages/MainPage.jsx
+++ frontend/src/component/Pages/MainPage.jsx
@@ -8,10 +8,15 @@ const LoginPage = () => {
         navigate("/login");
     }
 
+    const handleClickPresentationButton = () => {
+        navigate("/presentation/create");
+    }
+
     return (
         <div style={{display: "flex", flexDirection: 'column', alignItems: 'center', justifyContent: 'center',minHeight: '100vh', gap: '30px'}}>
             <h2>MainPage</h2>
             <button onClick={handleClickLoginButton}>로그인</button>
+            <button onClick={handleClickPresentationButton}>발표 바로가기</button>
         </div>
     );
 }
